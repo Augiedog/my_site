7eb0b1b273faa25136ae4e5a8eb6c392a75e16b4
Augie Bren <Augie>
2021-11-10 00:21:38 -0600

    up too late

--- apps/Paint.js
+++ apps/Paint.js
@@ -1,17 +1,22 @@
 let brush = 'black'
+// You can change the brush variable your console to change color.
 const painting = document.querySelector('.painting')
-document.getElementById('brushColor').textContent = `${brush}`
+document.getElementById('brushColor').textContent = `Current Color is ${brush}`
 
 painting.addEventListener('click', function(e){
   e.target.style.backgroundColor = brush  
 })  
-// Now, you can change the selectedColor variable (either in your code or in your console) to change the color pixels turn when you click them.
 
 let palette = document.querySelectorAll('.color-choice')
 for(let i = 0; i < palette.length; i++){
   palette[i].addEventListener('click', function () {
     brush = palette[i].id
-     document.getElementById('brushColor').textContent = `${brush}`
+     document.getElementById('brushColor').textContent = `Current Color is ${brush}`
     console.log(brush)
   })
 }
+
+document.getElementById('goBack').addEventListener('click', function() {
+  window.history.back()
+  window.alert("Taking you Back!!")
+})
---
+++ apps/index.js
@@ -0,0 +1,4 @@
+document.getElementById('paintApp').addEventListener('click', function() {
+    window.location='/apps/Paint.html'
+    window.alert("To the Painting App!!")
+  })
