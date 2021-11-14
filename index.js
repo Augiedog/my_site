cacc215936c0d3a3ad37722d183cab949ee96735
Augie Bren <Augie>
2021-11-13 18:26:32 -0600

    ugh

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.getElementById('paintApp').addEventListener('click', function() {
-    window.location='./Paint.html'
+    window.location='Paint.html'
     window.alert("To the Painting App!!")
   })
