a7e1ec1e30fff501ecab046e6250992fff04d014
Augie Bren <Augie>
2022-01-08 16:28:16 -0600

    added Dice App

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ document.getElementById('paintApp').addEventListener('click', function() {
     window.location='Paint.html'
     window.alert("To the Painting App!!")
   })
+
+document.getElementById('diceApp').addEventListener('click', function() {
+  window.location='dice.html'
+  window.alert("Let's Throw Some Dice!!")
+})
