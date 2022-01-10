a0071d49ca9a41fd8057467b9e3b4a9977c77e6f
Augie Bren <Augie>
2022-01-10 14:52:34 -0600

    typo

--- chooseAdven.js
+++ chooseAdven.js
@@ -3,12 +3,12 @@ document.getElementById('chooseAdven').addEventListener('click', function() {
     if(firstAnswer === 'left'){
         let secondAnswer = window.prompt(`You come across a stray cat. 
             It scampers off down a small hole, just large enough for you to crawl through. 
-            Do you follow it, or continue on your path?`)
+            Do you "follow" it, or "continue" on your path?`)
         if(secondAnswer === 'follow'){
             let thirdAnswer = window.prompt(`You follow the cat to a colony of cats, 
                 nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. 
-                They are content with you staying, but you wonder if you should alert the world 
-                to this magical safe haven.`)
+                They are content with you staying"stay", but you wonder if you should alert the world 
+                to this magical safe haven."spread the word"`)
             if(thirdAnswer === 'stay'){
                 let fourthAnswer = window.prompt(`You live happily amongst the cats for the rest 
                     of your days.`)
@@ -19,35 +19,37 @@ document.getElementById('chooseAdven').addEventListener('click', function() {
             }
         }else if(secondAnswer === 'continue'){
             let thirdAnswer = window.prompt(`You come across a chamber that reaches upward to a
-                shining light above. There is a long, winding staircase, and a much quicker, but
-                rickety-looking ladder that leads up toward the light. Which do you take?`)
+                shining light above. There is a long, winding "staircase", and a much quicker, but
+                rickety-looking "ladder" that leads up toward the light. Which do you take?`)
             if(thirdAnswer === 'ladder'){
                 let fourthAnswer = window.prompt(`After ascending a few feet up the ladder, one of 
                     its rungs snaps, and you comedically fall through each of the rungs below. Sheepish,
                     you return home.`)
             }else if(thirdAnswer === 'staircase'){
-                let fourthAnswer = window.prompt('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
+                let fourthAnswer = window.prompt(`After ascending the staircase, you discover a shiny
+                    blue stone, which you take home and cherish forever.`)
             }
         }
         
     
     } else if(firstAnswer === 'right'){
         let secondAnswer = window.prompt(`You come across a snoring dragon. 
-    On the other side of him, you see a shiny chest of treasure"past the dragon". Another path would 
-    lead you "away from the dragon" altogether. Which path do you take?`)
+            On the other side of him, you see a shiny chest of treasure"past the dragon". Another path would 
+            lead you "away from the dragon" altogether. Which path do you take?`)
         if(secondAnswer === 'past the dragon'){
             let thirdAnswer = window.prompt(`The dragon wakes up and sits upright. You only have a
-             moment to respond, to stay or run:`)
+             moment to respond, to "stay" or "run":`)
             if(thirdAnswer === 'stay'){
                 let thirdAnswer = window.prompt(`You and the dragon have an uplifting conversation 
-                    about local politics and become lifelong friends.`)
+                    about local politics and become lifelong friends.
+                    The Dragon\'s name is Hank and he let\'s you ride him.`)
             }else if(thirdAnswer = 'run'){
                 let thirdAnswer = window.prompt(`Quickly, you run back to the cave\'s entrance.
                  Sheepish, you return home.`)
             }
         }else if(secondAnswer === 'away from the dragon'){
             let thirdAnswer = window.prompt(`After walking a while longer, you come across a shiny 
-                blue flower. It is so beautiful that you decide you must either draw it or pick it. 
+                blue flower. It is so beautiful that you decide you must either "draw it" or "pick it". 
                 Which do you do?`)
             if(thirdAnswer === 'draw it'){
                 let thirdAnswer = window.prompt(`You draw the flower, capturing only a fraction of 
